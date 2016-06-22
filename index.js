5e615a238b32d6ac2815f68225d4caac8970a196
Vinayak Mishra <viks@vnykmshr.com>
2016-06-22 13:19:50 +0530

    Package updates

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
  * IMGURI instance, can be used to programmatically generate base64 data-uri from files, local or remote
  */
 
-module.exports = require('./lib');
+module.exports = require('./lib/imguri');
--- test/index.js
+++ test/index.js
@@ -2,14 +2,14 @@ var imguri = require('../');
 
 /** Test Code --------------------------------------------------------------- */
 if (require.main === module) {
-    (function () {
-        var options = {
-            force: false
-        };
-        var paths = ['test/test.png', 'test/nofile'];
-        paths.push('http://www.vnykmshr.com/images/favicon.ico');
-        paths.push('http://www.vnykmshr.com/');
-        paths.push('http://www.vnykmshr.com/images/whois.png');
-        imguri.encode(paths, options, console.log);
-    })();
+  (function () {
+    var options = {
+      force: false
+    };
+    var paths = ['test/test.png', 'test/nofile'];
+    paths.push('http://www.vnykmshr.com/images/favicon.ico');
+    paths.push('http://www.vnykmshr.com/');
+    paths.push('http://www.vnykmshr.com/images/whois.png');
+    imguri.encode(paths, options, console.log);
+  })();
 }
